499a1c4a441c4862bba71112038a256b8562c366
Ben McHone <ben@mchone.dev>
2023-07-24 21:50:07 -0500

    Added billboard ad example

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ app.use(express.static('public'));
  * Simple server -> client messaging
  */
 app.get('/server-time/sse', async (req, res) => {
+	res.setHeader("Cache-Control", "no-cache")
 	res.setHeader("Content-Type", "text/event-stream")
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader("Connection", "keep-alive")
 
 	while (true) {
 		res.write(`event: date\ndata: ${new Date().toISOString()}\n\n`);
@@ -73,6 +76,45 @@ app.post('/chat/message', (req, res) => {
 	chatEmitter.emit('message', chatMessage);
 
 	res.redirect('/chat/');
+});
+
+
+/**
+ * Ads
+ */
+
+function makeAd(title, subtitle, imgSrc, imgAlt) {
+	return {
+		title,
+		subtitle,
+		imgSrc,
+		imgAlt
+	}
+}
+
+let ads = [
+	makeAd('Source Allies', 'The preferred workplace for collaborative technologists seeking mastery', 'https://www.sourceallies.com/img/dark-jumbotron/home.png', 'Technologist working at a laptop'),
+	makeAd('Google', 'Google it!', 'https://lh3.googleusercontent.com/d_S5gxu_S1P6NR1gXeMthZeBzkrQMHdI5uvXrpn3nfJuXpCjlqhLQKH_hbOxTHxFhp5WugVOEcl4WDrv9rmKBDOMExhKU5KmmLFQVg', 'Google logo'),
+	makeAd('Twitter', 'Who was supposed to pay rent?', 'https://miro.medium.com/v2/resize:fit:4800/format:webp/1*u1atDFEKcHCLwCVB-dGysQ.jpeg', 'a headstone for twitter'),
+]
+
+app.get('/billboard/sse', async (req, res) => {
+	res.setHeader("Cache-Control", "no-cache")
+	res.setHeader("Content-Type", "text/event-stream")
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader("Connection", "keep-alive")
+
+	res.flushHeaders();
+
+	let idx = 0
+
+	while (true) {
+		res.write(`event: ad\ndata: ${JSON.stringify(ads[idx])}\n\n`)
+		await new Promise(res => setTimeout(res, 5000))
+		if (req.socket.closed) break;
+		idx += 1
+		idx = idx % ads.length
+	}
 });
 
 app.listen(3000, () => console.log('Listening on http://0.0.0.0:3000'));
